65038f934bf57e415816fc75c5ee5fa0cbaf502a
Ariel Xavier Manfredi <ariel.xavier.manfredi@gmail.com>
2024-06-20 14:36:20 -0300

    a

--- js/memorymatching.js
+++ js/memorymatching.js
@@ -1,4 +1,4 @@
-// script.js
+
 document.addEventListener('DOMContentLoaded', () => {
     const gameBoard = document.getElementById('memoryMatchingBoard');
     const resetButton = document.getElementById('resetButtonMemoryMatching');
